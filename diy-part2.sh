926a8b5b8b94ad6c85df6801e133210363eaea89
yudiaguspratama-netizen <yudiaguspratama@gmail.com>
2026-03-17 11:17:20 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-
-# 1. Atur IP Default ke 192.168.1.1 (Ubah jika perlu)
-sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-
-# 2. AKTIFKAN WIFI ON DEFAULT (Crucial!)
-# Menghapus 'disabled 1' agar WiFi langsung memancar setelah flash
+# WiFi ON
 sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 3. Optimasi RAM 64MB (ZRAM & Logging)
-# Mengecilkan ukuran log di RAM agar tidak penuh
-sed -i 's/log_size/log_size 512/g' package/base-files/files/bin/config_generate
+# Paksa hapus aplikasi yang menyebabkan segfault (jika terbawa feed)
+rm -rf package/feeds/small/luci-app-fchomo
+rm -rf package/feeds/small/luci-app-homeproxy
 
-# 4. Memastikan dependencies EQOS ikut terpilih
+# Tambahkan config EQOS
 echo "CONFIG_PACKAGE_luci-app-eqos=m" >> .config
-echo "CONFIG_PACKAGE_kmod-sched-core=y" >> .config
-echo "CONFIG_PACKAGE_tc-tiny=y" >> .config
